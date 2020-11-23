4fd0c9987ce9be20853e02c19ebe0715efd792a7
flower <flower@apap04.com>
2020-11-22 20:45:08 -0500

    add ban cog + rm tests
    Signed-off-by: flower <flower@apap04.com>

--- cogs/mod.py
+++ cogs/mod.py
@@ -17,7 +17,7 @@ class Mod(commands.Cog):
     @commands.guild_only()
     async def kick_user(self, ctx, member: discord.Member, *, reason=None) -> None:
         try:
-            if await perms.check_priv(ctx, member):
+            if await perms.check_priv(ctx, member=member):
                 return
             await member.kick(reason=default.responsible(ctx.author, member))
             await ctx.send(
@@ -25,14 +25,24 @@ class Mod(commands.Cog):
                 if reason is None
                 else f"{ctx.author} kicked {str(member)} for \"{reason}\"."
             )
-        except Exception:             
+        except commands.errors.BotMissingPermissions:             
             await ctx.send("i can't do that.")
             
     @commands.command(name="ban")
     @commands.has_guild_permissions(ban_members=True)
     @commands.guild_only()
     async def ban_user(self, ctx, member: discord.Member, *, reason=None) -> None:
-        pass
+        try:
+            if await perms.check_priv(ctx, member=member):
+                return
+            await member.ban(reason=default.responsible(ctx.author, member))
+            await ctx.send(
+                f"{ctx.author} banned {str(member)} for no reason"
+                if reason is None
+                else f"{ctx.author} banned {str(member)} for \"{reason}\"."
+            )
+        except commands.errors.BotMissingPermissions:
+            await ctx.send("i can't do that.")
 
     #endregion
 
