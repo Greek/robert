fbc5aedccccba84e4fa0f8785b4a864666f50b65
flower <flower@apap04.com>
2020-11-23 22:07:15 -0500

    use minimal help command
    Signed-off-by: flower <flower@apap04.com>

--- utils/data.py
+++ utils/data.py
@@ -2,7 +2,7 @@ import discord
 import os
 import logging
 
-from discord.ext.commands import AutoShardedBot, DefaultHelpCommand
+from discord.ext.commands import AutoShardedBot, MinimalHelpCommand
 from utils import perms
 
 class Bot(AutoShardedBot):
@@ -22,12 +22,20 @@ class Bot(AutoShardedBot):
             )
 
 
-class HelpFormat(DefaultHelpCommand):
+class HelpFormat(MinimalHelpCommand):
     def get_destination(self, no_pm: bool = False):
         if no_pm:
             return self.context.channel
         else:
             return self.context.author
+
+    def get_ending_note(self):
+        command_name = self.invoked_with
+        return "You can get help with a specific command with \"{0}{1} <cmd>\"\n" \
+            "It provides more information on what you can do with that command.".format(self.clean_prefix, command_name)
+    
+    def get_opening_note(self):
+        pass
 
     async def send_error_message(self, error):
         destination = self.get_destination(no_pm=True)
