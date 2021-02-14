61061f5742815beb871d628eba00fd9c5c65b898
flower <flower@apap04.com>
2021-02-14 03:05:38 -0500

    licensing and some other bull
    i'll just use branches  HH
    
    Signed-off-by: Andreas Papacharalampous <andreas@apap04.com>

--- cogs/about.py
+++ cogs/about.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2021-present flower and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from discord.ext import commands
 from utils import default
 import discord
@@ -41,7 +61,7 @@ class About(commands.Cog):
             embed.add_field(name="RAM usage", value=f"{ram_usage:.2f} MB", inline=True)
 
             await ctx.send(content=f"about **{ctx.bot.user}**", embed=embed)
-        except Exception:
+        except Exception as e:
             pass
 
 def setup(bot):
--- cogs/mod.py
+++ cogs/mod.py
@@ -1,5 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2021-present flower and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from utils.default import traceback_maker
 import discord
 from discord.ext import commands
@@ -55,7 +75,7 @@ class Mod(commands.Cog):
     @commands.command(name="purge")
     @commands.has_guild_permissions(manage_messages=True)
     @commands.guild_only()
-    async def mass_delete(self, ctx, amount: int, target: discord.Member = None):
+    async def mass_delete(self, ctx, amount: int):
         """ Mass delete messages at once. """
         try:
             await ctx.channel.purge(limit=amount)
