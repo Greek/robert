db914fc5b1bc0970c2a9a2c95ffad0b4686b3e4c
Andreas Papacharalampous <andreas@apap04.com>
2021-08-29 14:50:38 -0400

    FunSlash: uhh?
    Signed-off-by: Andreas Papacharalampous <andreas@apap04.com>

--- cogs/fun_slash.py
+++ cogs/fun_slash.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
+from discord import PartialEmoji, Emoji
 from discord.ext import commands
-from dislash import slash_command, Option, OptionType
+from discord.ext.commands import EmojiConverter
+from dislash import slash_command, Option, OptionType, ActionRow, Button, ButtonStyle
 import discord
 
 from utils import default
@@ -24,11 +25,11 @@ class FunSlash(commands.Cog):
         try:
             member = member or ctx.author
             embed = discord.Embed(color=member.color, description=f"{member.mention}")
-            embed.set_author(name=str(member), icon_url=member.avatar)
-            embed.set_thumbnail(url=member.avatar)
+            embed.set_author(name=str(member), icon_url=member.avatar_url)
+            embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name="Join date", value=f"{member.joined_at}"[0:10])
             embed.add_field(name="Creation date", value=f"{member.created_at}"[0:10])
-            embed.add_field(name="Roles", value=", ".join([r.mention for r in member.roles]), inline=False)
+            embed.add_field(name="Roles", value=", ".join([r.mention for r in member.roles[1:]]), inline=False)
             embed.set_footer(text="ID: " + str(member.id))
             await ctx.send(embed=embed)
         except Exception as e:
