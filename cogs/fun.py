0304ad906ae0b0a1fff94602530efd2c4532e868
Andreas Papacharalampous <andreas@apap04.com>
2021-08-29 10:45:02 -0400

    fix user command..
    jesus
    
    Signed-off-by: Andreas Papacharalampous <andreas@apap04.com>

--- cogs/fun.py
+++ cogs/fun.py
@@ -38,23 +38,23 @@ class Fun(commands.Cog):
         await ctx.send("you know what **screams** insecure?\n\n\"http://\"!")
 
     @commands.command(name="user")
-    async def get_user_info(self, ctx, member=discord.Member):
+    async def get_user_info(self, ctx, member: discord.Member = None):
         """
         Get information for a specific user.
         """
         try:
             if member is None:
                 member = ctx.author
-            embed = discord.Embed(description=f"{member.mention}")
+            embed = discord.Embed(colour=member.color, description=f"{member.mention}")
             embed.set_author(name=str(member), icon_url=member.avatar_url)
             embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name="Join date", value=f"{member.joined_at}"[0:10])
             embed.add_field(name="Creation date", value=f"{member.created_at}"[0:10])
-            embed.add_field(name="Roles", value=", ".join([r.mention for r in member.roles]), inline=False)
+            embed.add_field(name="Roles", value=", ".join([r.mention for r in member.roles[1:]]), inline=False)
             embed.set_footer(text="ID: " + str(member.id))
             await ctx.send(embed=embed)
         except Exception as e:
-            await ctx.send(default.traceback_maker(e))
+            await ctx.send(e)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
