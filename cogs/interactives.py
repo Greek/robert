2d4452939bbae0c1d18c67e5bf9a4b936e0ce5dc
Andreas Papacharalampous <andreas@apap04.com>
2021-08-31 04:39:01 -0400

    i18n: finish up strings
    Signed-off-by: Andreas Papacharalampous <andreas@apap04.com>

--- cogs/interactives.py
+++ cogs/interactives.py
@@ -3,6 +3,7 @@ import dislash
 from discord import Embed
 from discord.ext import commands
 import discord
+from utils.default import translate as _
 from dislash import slash_command, Option, OptionType
 
 from utils import default
@@ -14,14 +15,11 @@ class Interactives(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name="ip", description="What's the IP?",
+    @slash_command(name="ip", description=_("interactives.ip.desc"),
                    options=[Option("mention", "Mention a user and make the message public", OptionType.USER)])
     @dislash.has_guild_permissions(manage_messages=True)
     async def ip(self, inter, mention):
-        embed = default.branded_embed(title="What's the IP?", description=
-        "Pyrelic is currently under development, " +
-        "a release date will be announced in #announcements, " +
-        "so keep an eye out!")
+        embed = default.branded_embed(title=_("interactives.ip.desc"), description=_("interactives.ip.res"))
         if mention:
             await inter.respond(f"{mention.mention}", embed=embed, ephemeral=False)
         else:
@@ -31,10 +29,7 @@ class Interactives(commands.Cog):
     async def on_slash_command_error(self, inter, error):
         if isinstance(error, discord.errors.NotFound):
             pass # Ignore exception.
-        embed = default.branded_embed(title="What's the IP?", description=
-        "Pyrelic is currently under development, " +
-        "a release date will be announced in #announcements, " +
-        "so keep an eye out!")
+        embed = default.branded_embed(title="What's the IP?", description=_("interactives.ip.res"))
         await inter.respond(embed=embed, ephemeral=True)
 
 def setup(bot):
--- cogs/mod.py
+++ cogs/mod.py
@@ -37,52 +37,49 @@ class Mod(commands.Cog):
     def check_msg(ctx, m):
         return m.author == ctx.user
 
-    @commands.command(name="kick")
+    @commands.command(name="kick", description=_("cmds.kick.desc"))
     @commands.has_guild_permissions(kick_members=True)
     @commands.guild_only()
     async def kick_user(self, ctx, member: discord.Member, *, reason=None) -> None:
-        """ Kick a user, with an optional reason. """
         try:
             if await perms.check_priv(ctx, member=member):
                 return
             await member.kick(reason=default.responsible(ctx.author, reason))
             sent = await ctx.reply(
-                f"successfully kicked {str(member)} for no reason."
+                _("cmds.kick.res_noreason", member=str(member))
                 if reason is None
-                else f"successfully kicked {str(member)} for \"{reason}\"."
+                else _("cmds.kick.res_reason", member=str(member), reason=reason)
             )
             await sent.delete(delay=3)
         except discord.HTTPException:
             sent = await ctx.reply(_("events.missing_permission"))
             await sent.delete(delay=3)
             
-    @commands.command(name="ban")
+    @commands.command(name="ban", description=_("cmds.ban.desc"))
     @commands.has_guild_permissions(ban_members=True)
     @commands.guild_only()
     async def ban_user(self, ctx, member: discord.Member, *, reason=None) -> None:
-        """ Ban a user, with an optional reason. """
         try:
             if await perms.check_priv(ctx, member=member):
                 return
             await member.ban(reason=default.responsible(ctx.author, reason))
-            sent = await ctx.send(
-                f"successfully banned {str(member)} for no reason."
+            sent = await ctx.reply(
+                _("cmds.ban.res_noreason", member=str(member))
                 if reason is None
-                else f"successfully banned {str(member)} for \"{reason}\"."
+                else _("cmds.ban.res_reason", member=str(member), reason=reason)
             )
             await sent.delete(delay=3)
         except discord.HTTPException:
             sent = await ctx.reply(_("events.missing_permission"))
             await sent.delete(delay=3)
 
-    @commands.command(name="purge")
+    @commands.command(name="purge", description=_("cmds.purge.desc"))
     @commands.has_guild_permissions(manage_messages=True)
     @commands.guild_only()
     async def mass_delete(self, ctx, amount: int):
-        """ Mass delete messages at once. """
         try:
             await ctx.channel.purge(limit=amount + 1)
-            sent = await ctx.send(f"purged {amount} messages")
+            sent = await ctx.send(_("cmds.purge.res", amount=amount))
             await sent.delete(delay=3)
         except discord.HTTPException:
             sent = await ctx.reply(_("events.missing_permission"))
