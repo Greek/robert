e122bbdb511a702de7a6c084921d6e5bc3b77ccf
Andreas <me@apap04.com>
2022-04-04 17:15:09 +0000

    init snipe (wip)

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -5,7 +5,7 @@ from nextcord.ext import commands
 from nextcord.ext.commands import Context, errors
 from nextcord import Message
 from nextcord.ext import commands
-from utils import default
+from utils import default, perms
 from utils.default import translate as _
 
 
@@ -14,32 +14,49 @@ class Snipe(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.snipe = Message
+        self.snipe_author = {}
+        self.snipe_author_avatar = {}
+        self.snipe_content = {}
+        self.snipe_channel = {}
+        self.snipe_message = {}
+
+    snipe_author = {}
+    snipe_author_avatar = {}
+    snipe_content = {}
 
     @commands.Cog.listener()
     async def on_message_delete(self, message: Message):
-        self.snipe = message
+        self.snipe_author[message.channel.id] = message.author
+        self.snipe_author_avatar[message.channel.id] = message.author.avatar
+        self.snipe_content[message.channel.id] = message.content
 
-    @commands.command(name="snipe", description=_("cmds.snipe.desc"))
+    @commands.check(perms.only_owner)
+    @commands.command(name="snipe", aliases=['s'], description=_("cmds.snipe.desc"))
     @commands.cooldown(rate=1, per=4.0)
     async def get_last_deleted_message(self, ctx: Context):
         try:
+            snipe_author = self.snipe_author[ctx.channel.id]
+            snipe_avatar = self.snipe_author_avatar[ctx.channel.id]
+            snipe_content = self.snipe_content[ctx.channel.id]
+
             embed = default.branded_embed(
-                description=str(self.snipe.content),
+                description=str(snipe_content),
                 color=nextcord.Embed.Empty,
                 inline=True,
             )
 
             embed.set_author(
-                name=str(self.snipe.author),
-                icon_url=self.snipe.author.avatar,
+                name=str(snipe_author),
+                icon_url=snipe_author_avatar,
             )
-            embed.set_footer(text=f"#{str(self.snipe.channel.name)}")
-            embed.timestamp = self.snipe.created_at
+            embed.set_footer(text=f"#{str(self.snipe_message.channel.name)}")
+            embed.timestamp = self.snipe_message.created_at
 
+            print(snipe)
             return await ctx.send(embed=embed)
-        except:
-            return await ctx.send(_('cmds.snipe.failed'))
+        except Exception as e:
+            print(e)
+            # return await ctx.send(_('cmds.snipe.failed'))
 
     @nextcord.slash_command(
         guild_ids=[os.environ.get('DISCORD_GUILDID')],
