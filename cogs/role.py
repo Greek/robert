f504b9ba45e77d72f463ee323eeba70e36eaa140
Andreas <a@yeat.dev>
2022-05-23 14:32:03 +0000

    role: add error message
    Signed-off-by: Greek <a@yeat.dev>

--- cogs/role.py
+++ cogs/role.py
@@ -3,7 +3,7 @@ import nextcord
 from nextcord.ext import commands
 
 from utils.default import translate as _
-from utils.embed import success_embed_ephemeral
+from utils.embed import success_embed_ephemeral, warn_embed_ephemeral
 
 
 class Role(commands.Cog):
@@ -16,27 +16,29 @@ class Role(commands.Cog):
     @commands.has_permissions(manage_roles=True)
     @commands.bot_has_permissions(manage_roles=True)
     async def _role(self, ctx: commands.Context, user: nextcord.Member, role: str):
-        indexed_role = nextcord.utils.get(ctx.guild.roles, name=role)
+        try:
+            indexed_role = nextcord.utils.get(ctx.guild.roles, name=role)
 
-        if indexed_role in user.roles:
-            await user.remove_roles(indexed_role)
-            return await ctx.send(
-                embed=success_embed_ephemeral(
-                    _(
-                        "cmds.role.set.res.success_remove",
-                        role=role,
-                        user=user.mention,
+            if indexed_role in user.roles:
+                await user.remove_roles(indexed_role)
+                return await ctx.send(
+                    embed=success_embed_ephemeral(
+                        _(
+                            "cmds.role.set.res.success_remove",
+                            role=role,
+                            user=user.mention,
+                        )
                     )
                 )
+                
+            await user.add_roles(indexed_role)
+            return await ctx.send(
+                embed=success_embed_ephemeral(
+                    _("cmds.role.set.res.success", role=role, user=user.mention)
+                )
             )
-
-        await user.add_roles(indexed_role)
-        return await ctx.send(
-            embed=success_embed_ephemeral(
-                _("cmds.role.set.res.success", role=role, user=user.mention)
-            )
-        )
-
+        except:
+            return await ctx.send(embed=warn_embed_ephemeral(_("cmds.role.set.res.error", user=user.mention)))
 
 def setup(bot):
     bot.add_cog(Role(bot))
