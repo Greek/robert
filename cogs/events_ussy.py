4839aff331c32a02ef0b30c28a50532cbf704df8
Andreas <a@yeat.dev>
2022-07-11 12:08:54 -0400

    add new role to ussy members.
    Signed-off-by: Andreas <a@yeat.dev>

--- cogs/events_ussy.py
+++ cogs/events_ussy.py
@@ -11,7 +11,10 @@ class EventsUssy(commands.Cog):
     async def on_member_join(self, member: nextcord.Member):
         if member.guild.id == 950592148816945163:
             try:
+                ussy_role = member.guild.get_role(977704324840951878)
+
                 await member.edit(nick=member.name[:3] + "ussy")
+                await member.add_roles(ussy_role, reason="New member joined")
             except Exception as e:
                 print(e)
 
