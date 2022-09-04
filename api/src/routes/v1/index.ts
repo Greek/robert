f3191209e046db14802e8d0f9a209472788ddac3
Andreas <me@apap04.com>
2022-09-04 00:11:13 -0400

    api: stuff:
    Signed-off-by: Andreas <me@apap04.com>

--- api/src/routes/v1/index.ts
+++ api/src/routes/v1/index.ts
@@ -2,12 +2,10 @@ import express from 'express';
 
 import ping from './endpoints/ping';
 import teapot from './endpoints/teapot';
-import guild from './endpoints/guild/index';
 
 const v1route = express.Router();
 
 v1route.use('/ping', ping);
 v1route.use('/teapot', teapot);
-v1route.use('/guild', guild);
 
 export default v1route;
