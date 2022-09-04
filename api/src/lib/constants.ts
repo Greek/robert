bd6f9d7d8d53129dfd8723ea24a2edd499c6a773
Andreas <me@apap04.com>
2022-09-04 16:53:31 -0400

    we back.
    Signed-off-by: Andreas <me@apap04.com>

--- api/src/lib/constants.ts
+++
@@ -1,6 +0,0 @@
-export const API_NAME = 'Toilet';
-export const API_VERSION_API = 'v1';
-export const API_VERSION_SEMVER = '0.0.0';
-
-// Not used as of now
-export const API_RATELIMIT_TIMEOUT_MILLIS = 200000;
--- dashboard/src/modules/tlx/README.md
+++
@@ -1,4 +0,0 @@
-Telemetry Logging eXaminer (tlx://)
-
-Gets information about a server and spits out results through a webpage.
-This module's code is found here :-)
