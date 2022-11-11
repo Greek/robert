be7ac6f5e4b6381e69cb3a75230834bb93c6a631
Andreas <me@apap04.com>
2022-11-11 01:20:07 -0500

    Add events for lma
    Signed-off-by: Andreas <me@apap04.com>

---
+++ cogs/events_lma_autorole.py
@@ -0,0 +1,21 @@
+from nextcord.ext import commands
+import nextcord
+
+from utils.data import Bot
+
+
+class EventsLmaAutorole(commands.Cog):
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: nextcord.Member):
+        if member.guild.id != 1010560422283325590:
+            return
+        lma_role = member.guild.get_role(1040437387962093661)
+
+        return await member.add_roles(lma_role.id)
+
+
+def setup(bot):
+    bot.add_cog(EventsLmaAutorole(bot))
